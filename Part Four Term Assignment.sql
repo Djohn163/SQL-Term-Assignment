14789139b7a07637ad80524f5189e3fcc44206f5
Daleshia Johnson <38226183+Djohn163@users.noreply.github.com>
2019-03-31 03:05:59 -0400

    Add files via upload

---
+++ Part Four Term Assignment.sql
@@ -0,0 +1,18 @@
+#Problem1
+#INSERT INTO employees_new (col_1, col_2, col_3)
+#VALUES (10004, 10005, 20001, 20002)
+
+#Problem2
+#INSERT INTO employees_new (emp_no,salary,from_date, to_date)
+#THEN SELECT first_name, birth_date
+#FROM salaries_old
+#WHERE gender IN ('F')
+
+#Problem3
+#UPDATE sal_table
+#SET to_date = to_date*1.10
+#WHERE to_date > 20
+
+#Problem4 
+#DELETE FROM employees
+#WHERE emp_no = 10006
